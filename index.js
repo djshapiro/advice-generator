2ab009df927e61149b65f4e3f235a77d41b9333d
Dan Shapiro <dshapiro@express.com>
2019-06-04 13:12:42 -0400

    just text

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+// Start with order-1 questions
+
+// Then do order-1 answers
+
+// Then do order-2 questions
+
+// Then do order-2 answers
+
+// Then do order-n questions
+
+// Then do order-n answers
+
+// Finally, connect answers to questions. Pick a few words in the question and make sure they appear in the answer. Or maybe you could have a separate markov chain that maps questions with word X to answers, like it might alter the selection set of words for the answer. I'll have more insight on this when I get here.
